e043ae32977ae36f39fc4e2b128bf727d8a3f0e9
Immorta192 <WildStar1224@gmail.com>
2024-10-13 14:25:45 +0700

    Прикрутил бота с токеном, имя токена в файле в корневой папке

---
+++ bot.py
@@ -0,0 +1,35 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
+
+# Создаём основное приложение
+app = ApplicationBuilder().token("7360518240:AAEJ75gYh5IcuiS2tVWvSJfMIF35E7bf4jg").build()
+
+# Функция для старта бота
+async def start(update: Update, context):
+    keyboard = [
+        [
+            InlineKeyboardButton("Кнопка 1", callback_data='1'),
+            InlineKeyboardButton("Кнопка 2", callback_data='2'),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Выбери опцию:', reply_markup=reply_markup)
+
+# Функция для обработки нажатий кнопок
+async def button(update: Update, context):
+    query = update.callback_query
+    await query.answer()
+
+    # Ответ нажатия
+    if query.data == '1':
+        await query.edit_message_text(text="Ты нажал Кнопку 1")
+    elif query.data == '2':
+        await query.edit_message_text(text="Ты нажал Кнопку 2")
+
+# Добавляем обработчики
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(button))
+
+# Запуск приложения
+if __name__ == '__main__':
+    app.run_polling()
